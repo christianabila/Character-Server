ff2bb2c7391b401e8edeb13b505534644240a92f
christianabila <christian.abila@yahoo.com>
2015-10-04 19:15:12 +0200

    Added Multithreaded Server

---
+++ src/server/CharacterServer.java
@@ -0,0 +1,45 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+public class CharacterServer implements Runnable {
+
+	private DatagramSocket server;
+	private DatagramPacket p;
+	private Thread thread;
+
+	public CharacterServer(DatagramSocket server, DatagramPacket p)
+	{
+		this.server = server;
+		this.p = p;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			Random r = new Random();
+			
+			byte[] randomChar = ByteBuffer.allocate(4).putInt(r.nextInt(256)).array();
+			p = new DatagramPacket(randomChar, randomChar.length, p.getAddress(), p.getPort());
+			server.send(p);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void start()
+	{
+		if(thread == null)
+		{
+			thread = new Thread(this, "serverListeningThread");
+			thread.start();
+		}
+	}
+}
+
--- src/server/CharacterServerMain.java
+++ src/server/CharacterServerMain.java
@@ -2,8 +2,6 @@ package server;
 
 import java.io.IOException;
 import java.net.*;
-import java.nio.ByteBuffer;
-import java.util.Random;
 
 public class CharacterServerMain {
 
@@ -12,16 +10,18 @@ public class CharacterServerMain {
 		DatagramSocket server = null;
 		try {
 			server = new DatagramSocket(60000);
-			Random r = new Random();
-			
+
 			byte[] randomChar = new byte[4];
-			DatagramPacket p = new DatagramPacket(randomChar, randomChar.length);
-			server.receive(p);
-			
-			randomChar = ByteBuffer.allocate(4).putInt(r.nextInt(256)).array();
-			p = new DatagramPacket(randomChar, randomChar.length, p.getAddress(), p.getPort());
-			server.send(p);
-			
+			DatagramPacket p;
+
+			while(true)
+			{
+				p = new DatagramPacket(randomChar, randomChar.length);
+				server.receive(p);
+				CharacterServer s = new CharacterServer(server, p);
+				s.start();
+			}
+
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
